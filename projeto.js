b9ebe65011c9da463babba41329dea73d5fd8999
dorotrodrigues <dorotbarbosa57@gmail.com>
2022-10-08 13:17:14 -0300

    Parte 3 projeto intro web

--- projeto.js
+++ projeto.js
@@ -4,33 +4,42 @@ animal = {
     tempoDeVida:  18, 
     classe: "Mamífero",
     ambiente: "Doméstico",
-    extincao: false
+    extincao: true
 } 
 animal1 = {
     nome: "Mileide",
     idade: 6,
+    tempoDeVida:  17, 
+    classe: "Mamífero",
+    ambiente: "Doméstico",
     extincao: false
 }
 animal2 = {
     nome: "Mel",
     idade: 2,
+    tempoDeVida:  15, 
+    classe: "Mamífero",
+    ambiente: "Doméstico",
     extincao: true
 }
 animal3 = {
     nome: "Chiara",
     idade: 1,
+    tempoDeVida:  16, 
+    classe: "Mamífero",
+    ambiente: "Doméstico",
     extincao: false
 }
 
 const animais = []
 
-animais.push(animal)
-animais.push(animal1)
-animais.push(animal2)
-animais.push(animal3)
+// animais.push(animal)
+// animais.push(animal1)
+// animais.push(animal2)
+// animais.push(animal3)
 
 if(animal.extincao){
-    animais.push(animais)
+    animais.push(animal)
 } else {
     alert("Item não adicionado.")
 }
@@ -53,4 +62,35 @@ if(animal3.extincao){
     alert("Item não adicionado.")
 }
 
-console.log(animais)
+for(i in animais){
+    console.log(`Dados do animal ${i+1}: Nome: ${animais[i].nome} Idade: ${animais[i].idade} Está em extinção? ${animais[i].extincao}`)
+
+}
+
+function retornarDados(objeto){
+    return `Dados do animal: Nome: ${objeto.nome} Idade: ${objeto.idade} Está em extinção? ${objeto.extincao}`
+
+}
+
+console.log(retornarDados(animal))
+
+function retornaObjeto (arrayObjeto, nome){
+    let encontrou = false
+    let objetosEncontrados = []
+    let i = 0
+    for(bicho of arrayObjeto){
+        if (bicho.nome === nome) { 
+            encontrou = true
+            objetosEncontrados.push(bicho)
+         }
+    }
+    if(encontrou){
+        return objetosEncontrados
+    }
+    else{
+        alert (`Nenhum nome encontrado.`)
+    }
+}
+
+console.log(retornaObjeto(animais, "Kisha")) 
+// console.log(retornaObjeto(animais, "Bia"))
